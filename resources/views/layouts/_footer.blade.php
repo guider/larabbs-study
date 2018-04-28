f1687b66518482f1f6455c56d66b9a7894d51528
guider <guider@yeah.net>
2018-04-28 16:46:06 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    <div class="container">
+        <p class="pull-left">
+           By <a href="" targe="_blank">Guider</a> Design&&Code <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="pull-right"><a href="mailto:guider@yeah.net">联系我们</a></p>
+    </div>
+</div>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" data-toggle="collapse" data-target="#app-navbar-collapse"
+                    class="navbar-toggle collapsed">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a href="{{url('/')}}" class="navbar-brand">LaraBBS</a>
+
+        </div>
+        <div class="collapse navbar-collapse" id="app-navbar-collpose">
+            <ul class="nav navbar-nav"></ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="">登录</a></li>
+                <li><a href="">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
